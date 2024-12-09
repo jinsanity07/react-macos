56e6e0441816aeaf46e596ecc82b50c55f58ea80
jinsanity07git <31701385+jinsanity07git@users.noreply.github.com>
2024-12-09 11:47:58 -0500

    [style] use square icons

--- src/configs/launchpad.ts
+++ src/configs/launchpad.ts
@@ -2,57 +2,15 @@ import type { LaunchpadData } from "~/types";
 
 const launchpadApps: LaunchpadData[] = [
   {
-    id: "flint",
-    title: "Flint",
-    img: "img/icons/launchpad/flint.png",
-    link: "https://github.com/Renovamen/flint"
-  },
-  {
-    id: "oh-my-cv",
-    title: "Oh, My CV!",
-    img: "https://ohmycv.app/pwa-512x512.png",
-    link: "https://ohmycv.app"
-  },
-  {
-    id: "oh-vue-icons",
-    title: "Oh, Vue Icons!",
-    img: "img/icons/launchpad/oh-vue-icons.png",
-    link: "https://oh-vue-icons.js.org"
-  },
-  {
-    id: "gungnir",
-    title: "Gungnir",
-    img: "img/icons/launchpad/gungnir.png",
-    link: "https://vuepress-theme-gungnir.vercel.app/"
-  },
-  // {
-  //   id: "metallic",
-  //   title: "Metallic",
-  //   img: "img/icons/launchpad/meta.png",
-  //   link: "https://github.com/Renovamen/metallic"
-  // },
-  {
     id: "what-if",
     title: "My Notebook",
-    img: "https://jinsanity07git.github.io/UWMnotebook/src/svg/Mark_RGB_Yellow.svg",
+    img: "img/icons/launchpad/microsoft-onenote.avif",
     link: "https://jinsanity07git.github.io/UWMnotebook"
-  },
-  {
-    id: "cube",
-    title: "Cube Solver",
-    img: "img/icons/launchpad/cube.png",
-    link: "https://cube.zxh.io/"
-  },
-  {
-    id: "resume",
-    title: "Résumé",
-    img: "img/icons/launchpad/resume.png",
-    link: "https://resume.zxh.io/"
   },
   {
     id: "soil",
     title: "Soil Mechanics",
-    img: "https://jinsanity07git.github.io/UWMnotebook/UWM/img/soil%20mechanics.png",
+    img: "img/icons/launchpad/soilMechanics.png",
     link: "https://jinsanity07git.github.io/UWMnotebook/#/UWM/CE335/README"
   },
   {
