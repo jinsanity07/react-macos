0adcae875d950bd2fa4ea26616354b2606acc403
jinsanity07git <31701385+jinsanity07git@users.noreply.github.com>
2024-12-20 16:00:42 -0500

    [feat] change the default music

--- src/configs/music.ts
+++ src/configs/music.ts
@@ -1,10 +1,10 @@
 import type { MusicData } from "~/types";
 
 const music: MusicData = {
-  title: "Sunflower",
-  artist: "Post Malone / Swae Lee",
-  cover: "//p1.music.126.net/z0IO1vEsowL9mD_5yzUjeA==/109951163936068098.jpg",
-  audio: "music/sunflower.mp3"
+  title: "分分钟需要你",
+  artist: " 林子祥",
+  cover: "/music/fenfenzhong.png",
+  audio: "music/分分钟需要你 - 林子祥.mp3"
 };
 
 export default music;
